89d422ad43c847cd4726209cb39f3709f37ce61b
temamarvel <tema.denisoff@gmail.com>
2026-02-09 11:35:21 +0400

    add project template

---
+++ FillTheDoc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FillTheDoc
+//
+//  Created by Artem Denisov on 09.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FillTheDoc/FillTheDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillTheDocApp.swift
+//  FillTheDoc
+//
+//  Created by Artem Denisov on 09.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FillTheDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
